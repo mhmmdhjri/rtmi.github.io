23a1bf8c724abd7d1e7d0f5b08dcf17b6bdfd196
mhmmdhjri <muhammadhijri25@gmail.com>
2024-01-17 08:35:42 +0700

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+// nav fix
+
+// button
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click',function(){
+    hamburger.classList.toggle('active');
+    
+ navMenu.classList.toggle('hidden');
+  
+})
